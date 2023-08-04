68224fe8bc7ef287c2b3b95b73389cd382011b84
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-08-05 07:35:41 +0900

    update operatorAndLoop.kt

--- operatorAndLoop.kt
+++ operatorAndLoop.kt
@@ -29,4 +29,37 @@ var msg: String = if (p < 0) {
 println(msg)
 
 
+// when 式
+val p = 3.1
+var msg: String = when {
+       p < 0 -> "p is a negative number."
+       p == 0.0 -> "p = 0."
+       else -> "p is a positive number."
+}
+println(msg)
 
+
+val num = 5
+when (num) {
+       1 -> println("x = 1.")
+       2 -> println("x = 2.")
+       else -> { // 複数行で書くことも可能
+              println("NNNNNNN.")
+       }
+}
+
+val score = 80
+val msg = when (score) {
+       100,99,98,97,96,95 -> "Wonderful!"
+       in 80..95 -> "Great!"
+       in 60..79 -> "Nice!"
+       !in 60.. 100 -> "Failure."
+       else -> "Undefined!"
+}
+println(msg)
+
+val obj: Any = "aiueo"
+when (obj) {
+       is String -> println("${obj.length}")
+       else -> println("Type is not String")
+}
