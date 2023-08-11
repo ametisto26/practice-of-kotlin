b5f3d6819d9813943c1c1a69f2f354567c998002
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-08-12 08:18:25 +0900

    update

--- objectOriented.kt
+++ objectOriented.kt
@@ -3,11 +3,21 @@
 class Human {
        var name = "John Doe"
        var age = 23
+              set(value) { // 慣例的に変数名はvalueとする。
+                     if (value < 0) {
+                            println("不正な値です。")
+                     } else {
+                            field = value // field はバッキングフィールドのことである。
+                     }
+              }
 
-       protected fun intro() {
+       fun intro() {
               println("我が名は${name}，齢${age}である。")
        }
 }
 
 val human = Human()
 human.intro()
+human.age = -1
+println(human.name)
+
