98325670f3e9323fb18ebb2edf9179849e6c58e2
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-08-12 09:02:35 +0900

    update objectOriented.kt

--- objectOriented.kt
+++ objectOriented.kt
@@ -16,6 +16,63 @@ class Human {
        }
 }
 
+//プライマリコンストラクタ
+class Human constructor(name: String, age: Int) {
+       var name: String
+       var age: Int
+
+       init { // プロパティを設定する
+              this.name = name
+              this.age = age
+       }
+
+       fun intro() {
+              println("我が名は${name}，齢${age}である。")
+       }
+}
+
+//プライマリコンストラクタがアノテーションやアクセス修飾子を持たない場合は，以下のように省略可能である。
+// class Human(name: String, age: Int){  }
+
+
+//プライマリコンストラクタの引数にvar / val をつけることで
+//プロパティの宣言と初期化を同時に行うこともできる。
+class Human (var name: String, var age: Int) {
+       fun intro() {
+              println("我が名は${name}，齢${age}である。")
+       }
+}
+
+//アクセス修飾子を付与できる
+class Human (public var name: String, internal var age: Int) {
+       fun intro() {
+              println("我が名は${name}，齢${age}である。")
+       }
+}
+
+
+//セカンダリコンストラクタを利用する場合
+class Human (var name: String, var age: Int) {
+       constructor(name: String):this(name, 10) {} //1つめ
+       constructor():this("太郎") {} //2つめ
+       fun intro() {
+              println("我が名は${name}，齢${age}である。")
+       }
+}
+
+val human1 = Human("J.", 6)
+val human2 = Human("K.")
+val human3 = Human()
+
+
+//引数の既定値を設定する場合
+class Human (var name: String = "二郎", var age: Int = 10) {
+       fun intro() {
+              println("我が名は${name}，齢${age}である。")
+       }
+}
+
+
 val human = Human()
 human.intro()
 human.age = -1
