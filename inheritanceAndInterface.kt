0515cbdb348873f5f3bcde44cea0eacb213ce665
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-08-17 13:26:46 +0900

    update inheritanceAndInterface.kt

--- inheritanceAndInterface.kt
+++ inheritanceAndInterface.kt
@@ -1,5 +1,19 @@
-open class Human(var name: String) {
-       open fun intro() {
+open class Human(var name: String) { //継承可能な元のクラスには open をつける。
+       open fun intro() { //override可能なメソッドに open をつける。
               println("我が名は${this.name}。")
        }
 }
+
+class PerfectHuman(name: String, var place: String): Human(name) {
+       override fun intro() {
+              println("${this.name}! ${this.name}!")
+              super.intro()
+       }
+       fun liveIn() {
+              println("We live in ${this.place}")
+       }
+}
+
+val joji = PerfectHuman("Nakata", "Tokio")
+joji.liveIn()
+joji.intro()
