d1bd6cb7213fd7b81d1d5aeca0c004531404e120
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-08-26 04:36:46 +0900

    update dataclass.kt

--- dataclass.kt
+++ dataclass.kt
@@ -21,5 +21,29 @@ println(m1 == m2) // True
 println(m1) // 暗黙的に呼び出されている
 
 
+// componentN（分割代入）
+val t1 = Triple("James", "nojob", 25)
+val (name, job, age) = t1
+println(name)
+println(job)
+println(age)
+
+/* 内部的にはこのようになっている
+val t1 = Triple("James", "nojob", 25)
+val name = t1.component1()
+val job = t1.component2()
+val age = t1.component3()
+*/
 
 
+data class MemberCo(val name: String, val job: String, val age: Int)
+val member1 = MemberCo("Johnson", "nojob", 25)
+
+
+// copy メソッド（オブジェクトの複製）
+// data class MemberCo(val name: String, val job: String, val age: Int)
+// val member1 = MemberCo("Johnson", "nojob", 25)
+
+val member2 = member1.copy(age = 47)
+println(member1)
+println(member2)
