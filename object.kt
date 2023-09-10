71e37f19ba76cf8f6735f0f10196cc706d19762c
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-09-11 04:47:49 +0900

    update object.kt

--- object.kt
+++ object.kt
@@ -58,3 +58,55 @@ println(myc::class) // class MyClass
 const val date = 1
 
 
+class genericExample<T>(var value: T) {
+       fun getProp(): T {
+              return value
+       }
+}
+
+class genericExample2<T, T2>(var value: T, var value2: T2) {
+       fun getProp(): T {
+              return value
+       }
+}
+
+val g1 =genericExample<String>("foo")
+val g2 =genericExample<Int>(26)
+val g3 =genericExample("neko") // 型推論できるなら省略可能
+val g4 =genericExample2<String, Int>("s", 5)
+
+println(g1.getProp())
+println(g2.getProp())
+g1.value = 5 // エラー
+
+
+fun <T> tail(arr: Array<T>): T = arr[arr.size - 1]
+
+val aaa = arrayOf(1, 2, 6)
+
+println(tail<Int>(aaa))
+println(tail(aaa)) // 型推論できるなら省略可能
+
+
+
+//ネストクラス
+//クラスの中にクラスを定義できる
+
+class Outer(val name: String = "Outer") {
+       inner class Nested(val name: String = "Nested") {
+              fun show() = {
+                     println("${this@Outer.name} called show()")
+                     println("${name} is running")
+              }
+       }
+
+       fun run(){
+              val nested = Nested()
+              nested.show()
+       }
+}
+
+val o = Outer()
+o.run()
+
+
